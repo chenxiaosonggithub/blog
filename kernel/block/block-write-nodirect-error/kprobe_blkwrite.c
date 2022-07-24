62c22b2e6e8a4d5e86df6346386fc60937992792
ChenXiaoSong <chenxiaosongemail@foxmail.com>
2022-07-24 18:48:01 +0800

    Update notes
    Signed-off-by: ChenXiaoSong <chenxiaosongemail@foxmail.com>

--- kernel/block/block-write-nodirect-error/kprobe_blkwrite.c
+++ kernel/block/block-write-nodirect-error/kprobe_blkwrite.c
@@ -10,21 +10,91 @@
  * whenever kernel_clone() is invoked to create a new process.
  */
 
+#define pr_fmt(fmt) "%s: " fmt, __func__
+
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/syscalls.h>
+#include <linux/kprobes.h>
+
+#include <linux/bio.h>
+#include <linux/bitops.h>
+#include <linux/blkdev.h>
+#include <linux/completion.h>
+#include <linux/kernel.h>
+#include <linux/export.h>
+#include <linux/init.h>
+#include <linux/pci.h>
+#include <linux/delay.h>
+#include <linux/hardirq.h>
+#include <linux/scatterlist.h>
+#include <linux/blk-mq.h>
+#include <linux/ratelimit.h>
+#include <asm/unaligned.h>
+
+#include <scsi/scsi.h>
+#include <scsi/scsi_cmnd.h>
+#include <scsi/scsi_dbg.h>
+#include <scsi/scsi_device.h>
+#include <scsi/scsi_driver.h>
+#include <scsi/scsi_eh.h>
+#include <scsi/scsi_host.h>
+#include <scsi/scsi_transport.h> /* __scsi_init_queue() */
+#include <scsi/scsi_dh.h>
+
+#include <trace/events/scsi.h>
+
+#include <../drivers/scsi/scsi_debugfs.h>
+#include <../drivers/scsi/scsi_priv.h>
+#include <../drivers/scsi/scsi_logging.h>
 
 #define FILE_NAME "/root/chenxiaosong/blkwrite-error/file"
 #define EXPECT_FILE_SZ		(40*1024*1024)
 
+static char symbol[KSYM_NAME_LEN] = "scsi_dispatch_cmd";
+module_param_string(symbol, symbol, KSYM_NAME_LEN, 0644);
+
+/* For each probe you need to allocate a kprobe structure */
+static struct kprobe kp = {
+	.symbol_name	= symbol,
+};
+
 static char expect_buf[EXPECT_FILE_SZ];
 static struct file *file = NULL;
+
+/* kprobe pre_handler: called just before the probed instruction is executed */
+static int __kprobes handler_pre(struct kprobe *p, struct pt_regs *regs)
+{
+#ifdef CONFIG_X86
+#endif
+#ifdef CONFIG_ARM64
+#endif
+	/* A dump_stack() here will give a stack backtrace */
+	return 0;
+}
+
+/* kprobe post_handler: called after the probed instruction is executed */
+static void __kprobes handler_post(struct kprobe *p, struct pt_regs *regs,
+				unsigned long flags)
+{
+}
 
 static int __init kprobe_init(void)
 {
 	loff_t pos;
 	long res;
+	int ret;
+
+	kp.pre_handler = handler_pre;
+	kp.post_handler = handler_post;
+
+	ret = register_kprobe(&kp);
+	if (ret < 0) {
+		pr_err("register_kprobe failed, returned %d\n", ret);
+		return ret;
+	}
+	pr_info("Planted kprobe at %p\n", kp.addr);
 
 	file = filp_open(FILE_NAME, O_RDONLY, 0644);
 
@@ -50,6 +120,8 @@ static int __init kprobe_init(void)
 
 static void __exit kprobe_exit(void)
 {
+	unregister_kprobe(&kp);
+	pr_info("kprobe at %p unregistered\n", kp.addr);
 	if(file) {
 		filp_close(file, NULL);
 	}
