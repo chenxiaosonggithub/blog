93d9ea0290606be7f78cbec19a804bde28ccadbd
ChenXiaoSong <chenxiaosongemail@foxmail.com>
2022-07-25 22:00:32 +0800

    Update notes
    Signed-off-by: ChenXiaoSong <chenxiaosongemail@foxmail.com>

--- kernel/block/block-write-nodirect-error/kprobe_blkwrite.c
+++ kernel/block/block-write-nodirect-error/kprobe_blkwrite.c
@@ -180,15 +180,13 @@ static void check_scsi_data(struct scsi_cmnd *cmd, struct kprobe *p)
 static int __kprobes write_handler_pre(struct kprobe *p, struct pt_regs *regs)
 {
 	struct scsi_cmnd *cmd;
-	bool is_write;;
 #ifdef CONFIG_X86
 	cmd = (struct scsi_cmnd *)regs->di;
 #endif
 #ifdef CONFIG_ARM64
 	cmd = (struct scsi_cmnd *)regs->x0;
 #endif
-	is_write = scsi_is_write(cmd);
-	if (!is_write)
+	if (cmd->sc_data_direction != DMA_TO_DEVICE)
 		return 0;
 
 	if (!check_cmd(cmd)) {
@@ -204,15 +202,13 @@ static int __kprobes write_handler_pre(struct kprobe *p, struct pt_regs *regs)
 static int __kprobes read_handler_pre(struct kprobe *p, struct pt_regs *regs)
 {
 	struct scsi_cmnd *cmd;
-	bool is_read;
 #ifdef CONFIG_X86
 	cmd = (struct scsi_cmnd *)regs->di;
 #endif
 #ifdef CONFIG_ARM64
 	cmd = (struct scsi_cmnd *)regs->x0;
 #endif
-	is_read = scsi_is_read(cmd);
-	if (!is_read)
+	if (cmd->sc_data_direction != DMA_FROM_DEVICE)
 		return 0;
 
 	check_scsi_data(cmd, p);
