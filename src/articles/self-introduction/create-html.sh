ff07a7938b768285b15e17651de53b20145556be
ChenXiaoSong <chenxiaosongemail@foxmail.com>
2023-06-09 00:42:59 +0800

    Update notes
    Signed-off-by: ChenXiaoSong <chenxiaosongemail@foxmail.com>

--- src/articles/self-introduction/create-html.sh
+++ src/articles/self-introduction/create-html.sh
@@ -1,6 +1,8 @@
-sudo pandoc index.md -o /var/www/html/index.html --from markdown --to html --standalone --metadata encoding=gbk --metadata title="陈孝松个人主页" --toc
-sudo pandoc photos.md -o /var/www/html/self-introduction/photos.html --from markdown --to html --standalone --metadata encoding=gbk --metadata title="陈孝松照片" --toc
-sudo pandoc openharmony.md -o /var/www/html/self-introduction/openharmony.html --from markdown --to html --standalone --metadata encoding=gbk --metadata title="陈孝松OpenHarmony贡献" --toc
-sudo chown -R www-data:www-data /var/www/
-sudo find /var/www -type f -exec chmod 400 {} +
-sudo find /var/www -type d -exec chmod 500 {} +
+pandoc index.md -o /var/www/html/index.html --from markdown --to html --standalone --metadata encoding=gbk --metadata title="陈孝松个人主页" --toc
+pandoc photos.md -o /var/www/html/self-introduction/photos.html --from markdown --to html --standalone --metadata encoding=gbk --metadata title="陈孝松照片" --toc
+pandoc openharmony.md -o /var/www/html/self-introduction/openharmony.html --from markdown --to html --standalone --metadata encoding=gbk --metadata title="陈孝松OpenHarmony贡献" --toc
+rm /var/www/html/pictures -rf
+cp /home/sonvhi/chenxiaosong/code/pictures/pictures/ /var/www/html/ -rf
+chown -R www-data:www-data /var/www/
+find /var/www -type f -exec chmod 400 {} +
+find /var/www -type d -exec chmod 500 {} +
