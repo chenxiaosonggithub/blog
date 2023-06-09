8fb02e0f6b2d290491a7356010d08f0f636f5ba0
ChenXiaoSong <chenxiaosongemail@foxmail.com>
2023-06-09 10:59:32 +0800

    Update notes
    Signed-off-by: ChenXiaoSong <chenxiaosongemail@foxmail.com>

--- src/articles/self-introduction/create-html.sh
+++ src/articles/self-introduction/create-html.sh
@@ -1,8 +1,9 @@
-pandoc index.md -o /var/www/html/index.html --from markdown --to html --standalone --metadata encoding=gbk --metadata title="陈孝松个人主页" --toc
-pandoc photos.md -o /var/www/html/self-introduction/photos.html --from markdown --to html --standalone --metadata encoding=gbk --metadata title="陈孝松照片" --toc
-pandoc openharmony.md -o /var/www/html/self-introduction/openharmony.html --from markdown --to html --standalone --metadata encoding=gbk --metadata title="陈孝松OpenHarmony贡献" --toc
-rm /var/www/html/pictures -rf
-cp /home/sonvhi/chenxiaosong/code/pictures/pictures/ /var/www/html/ -rf
-chown -R www-data:www-data /var/www/
-find /var/www -type f -exec chmod 400 {} +
-find /var/www -type d -exec chmod 500 {} +
+mkdir -p /home/sonvhi/chenxiaosong/www/html/self-introduction/
+pandoc /home/sonvhi/chenxiaosong/code/blog/src/articles/self-introduction/index.md -o /home/sonvhi/chenxiaosong/www/html/index.html --from markdown --to html --standalone --metadata encoding=gbk --metadata title="陈孝松个人主页" --toc
+pandoc /home/sonvhi/chenxiaosong/code/blog/src/articles/self-introduction/photos.md -o /home/sonvhi/chenxiaosong/www/html/self-introduction/photos.html --from markdown --to html --standalone --metadata encoding=gbk --metadata title="陈孝松照片" --toc
+pandoc /home/sonvhi/chenxiaosong/code/blog/src/articles/self-introduction/openharmony.md -o /home/sonvhi/chenxiaosong/www/html/self-introduction/openharmony.html --from markdown --to html --standalone --metadata encoding=gbk --metadata title="陈孝松OpenHarmony贡献" --toc
+rm /home/sonvhi/chenxiaosong/www/html/pictures -rf
+cp /home/sonvhi/chenxiaosong/code/pictures/pictures/ /home/sonvhi/chenxiaosong/www/html/ -rf
+chown -R www-data:www-data /home/sonvhi/chenxiaosong/www/
+find /home/sonvhi/chenxiaosong/www -type f -exec chmod 400 {} +
+find /home/sonvhi/chenxiaosong/www -type d -exec chmod 500 {} +
---
+++ src/nginx/link.sh
@@ -0,0 +1,2 @@
+rm /etc/nginx/sites-enabled/default
+ln -s ${PWD}/nginx-config /etc/nginx/sites-enabled/default
