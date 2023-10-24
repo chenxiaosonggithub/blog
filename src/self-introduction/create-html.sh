55d32b07f500e6f4a4ce790dc697cb20e21957bd
ChenXiaoSong <chenxiaosongemail@foxmail.com>
2023-10-24 07:46:13 +0000

    Update notes
    Signed-off-by: ChenXiaoSong <chenxiaosongemail@foxmail.com>

--- src/self-introduction/create-html.sh
+++ src/self-introduction/create-html.sh
@@ -1,13 +1,24 @@
+rm /var/www/html/ -rf
+mkdir -p /var/www/html/
 mkdir -p /var/www/html/self-introduction/
 mkdir -p /var/www/html/nfs
 
-pandoc /home/sonvhi/chenxiaosong/code/blog/src/articles/self-introduction/index.md -o /var/www/html/index.html --from markdown --to html --standalone --metadata encoding=gbk --metadata title="陈孝松个人主页" --toc
-pandoc /home/sonvhi/chenxiaosong/code/blog/src/articles/self-introduction/photos.md -o /var/www/html/self-introduction/photos.html --from markdown --to html --standalone --metadata encoding=gbk --metadata title="陈孝松照片" --toc
-pandoc /home/sonvhi/chenxiaosong/code/blog/src/articles/self-introduction/openharmony.md -o /var/www/html/self-introduction/openharmony.html --from markdown --to html --standalone --metadata encoding=gbk --metadata title="陈孝松OpenHarmony贡献" --toc
-pandoc /home/sonvhi/chenxiaosong/code/blog/src/fs/nfs/nfs-null-ptr-in-nfs_updatepage.md -o /var/www/html/nfs/nfs-null-ptr-in-nfs_updatepage.html --from markdown --to html --standalone --metadata encoding=gbk --metadata title="4.19 nfs_updatepage空指针解引用问题" --toc
+# --standalone：此选项指示 pandoc 生成一个完全独立的输出文件，包括文档标题、样式表和其他元数据，使输出文件成为一个完整的文档。
+# --metadata encoding=gbk：这个选项允许您添加元数据。在这种情况下，您将 encoding 设置为 gbk，指定输出 HTML 文档的字符编码为 GBK。这对于确保生成的文档以正确的字符编码进行保存非常重要。
+# --toc：这个选项指示 pandoc 生成一个包含文档目录（Table of Contents，目录）的 HTML 输出。TOC 将包括文档中的章节和子章节的链接，以帮助读者导航文档。
+pandoc /home/sonvhi/chenxiaosong/code/blog/src/self-introduction/index.md -o /var/www/html/index.html --metadata title="陈孝松个人主页" --from markdown --to html --standalone --metadata encoding=gbk --toc
+pandoc /home/sonvhi/chenxiaosong/code/blog/src/self-introduction/photos.md -o /var/www/html/self-introduction/photos.html --metadata title="陈孝松照片" --from markdown --to html --standalone --metadata encoding=gbk --toc
+pandoc /home/sonvhi/chenxiaosong/code/blog/src/self-introduction/openharmony.md -o /var/www/html/self-introduction/openharmony.html --metadata title="陈孝松OpenHarmony贡献" --from markdown --to html --standalone --metadata encoding=gbk --toc
+pandoc /home/sonvhi/chenxiaosong/code/blog/src/nfs/nfs-null-ptr-in-nfs_updatepage.md -o /var/www/html/nfs/nfs-null-ptr-in-nfs_updatepage.html --metadata title="4.19 nfs_updatepage空指针解引用问题" --from markdown --to html --standalone --metadata encoding=gbk --toc
 
-rm /var/www/html/pictures -rf
+# pictures是我的私有仓库
 cp /home/sonvhi/chenxiaosong/code/pictures/pictures/ /var/www/html/ -rf
+
 chown -R www-data:www-data /var/www/
+
+# -type f：这个选项告诉 find 只搜索普通文件（不包括目录和特殊文件）。
+# -exec chmod 400 {} +：这个部分告诉 find 对每个找到的文件执行 chmod 400 操作。{} 表示找到的文件的占位符，+ 表示一次处理多个文件以提高效率。
 find /var/www -type f -exec chmod 400 {} +
+
+# -type d：这个选项告诉find只搜索目录（不包括普通文件）。
 find /var/www -type d -exec chmod 500 {} +
