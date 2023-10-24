f3144327b3cc2a3b7048bad5946099915cb8d6f4
ChenXiaoSong <chenxiaosongemail@foxmail.com>
2023-10-24 16:24:43 +0800

    Update notes
    Signed-off-by: ChenXiaoSong <chenxiaosongemail@foxmail.com>

--- src/chenxiaosong.com/link.sh
+++ src/chenxiaosong.com/link.sh
@@ -1,2 +1,3 @@
 rm /etc/nginx/sites-enabled/default
-ln -s ${PWD}/nginx-config /etc/nginx/sites-enabled/default
+# /home/sonvhi/chenxiaosong/code/blog 请替换为具体的路径
+ln -s /home/sonvhi/chenxiaosong/code/blog/src/chenxiaosong.com/nginx-config /etc/nginx/sites-enabled/default
