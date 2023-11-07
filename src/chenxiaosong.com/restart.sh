bc3c75e9dd15e5daec53440aeebedf46f457b150
ChenXiaoSong <chenxiaosongemail@foxmail.com>
2023-11-07 15:30:08 +0800

    Update notes
    Signed-off-by: ChenXiaoSong <chenxiaosongemail@foxmail.com>

--- src/chenxiaosong.com/restart.sh
+++ src/chenxiaosong.com/restart.sh
@@ -9,6 +9,8 @@ cd ${HOME}
 
 sudo bash ${src_path}blog/src/chenxiaosong.com/link.sh
 sudo bash ${src_path}blog/src/chenxiaosong.com/create-html.sh
-# sudo find ${dst_path}html/ -type f -exec sed -i 's/chenxiaosong.com/172.20.26.131/g' {} + # 如果部署在局域网，替换成局域网ip
+# 如果部署在局域网，替换成局域网ip
+# sudo find ${dst_path}html/ -type f -exec sed -i 's/chenxiaosong.com/172.20.26.131/g' {} +
+# sudo sed -i 's/chenxiaosong.com/172.20.26.131/g' /etc/nginx/sites-enabled/default
 # sudo iptables -F # 根据情况决定是否要清空防火墙规则
 sudo systemctl restart nginx
