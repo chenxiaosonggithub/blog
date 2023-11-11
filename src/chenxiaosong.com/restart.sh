73c458c9090cf2fea9e600a05a54bd8ce5ce4946
ChenXiaoSong <chenxiaosongemail@foxmail.com>
2023-11-11 10:29:21 +0000

    Update notes
    Signed-off-by: ChenXiaoSong <chenxiaosongemail@foxmail.com>

--- src/chenxiaosong.com/restart.sh
+++ src/chenxiaosong.com/restart.sh
@@ -1,5 +1,6 @@
 src_path=/home/sonvhi/chenxiaosong/code/
 dst_path=/var/www/
+repalace_ip=172.20.26.131
 
 cd ${src_path}pictures/
 git pull origin master
@@ -7,11 +8,11 @@ cd ${src_path}blog/
 git pull origin master
 cd ${HOME}
 
-sudo bash ${src_path}blog/src/chenxiaosong.com/link.sh
-sudo bash ${src_path}blog/src/chenxiaosong.com/create-html.sh
+bash ${src_path}blog/src/chenxiaosong.com/link.sh
+bash ${src_path}blog/src/chenxiaosong.com/create-html.sh
 # # 如果部署在局域网，替换成局域网ip
-# sudo find ${dst_path}html/ -type f -exec sed -i 's/chenxiaosong.com/172.20.26.131/g' {} +
+# find ${dst_path}html/ -type f -exec sed -i 's/chenxiaosong.com/'${repalace_ip}'/g' {} +
 # # default文件本来是个软链接，执行完sed后变成了文件
-# sudo sed -i 's/chenxiaosong.com/172.20.26.131/g' /etc/nginx/sites-enabled/default
-# sudo iptables -F # 根据情况决定是否要清空防火墙规则
-sudo systemctl restart nginx
+# sed -i 's/chenxiaosong.com/'${repalace_ip}'/g' /etc/nginx/sites-enabled/default
+# iptables -F # 根据情况决定是否要清空防火墙规则
+service nginx restart
