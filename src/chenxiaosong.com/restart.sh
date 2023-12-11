eac762c573474fe85bcecc47a5d51e50f49f7b57
ChenXiaoSong <chenxiaosongemail@foxmail.com>
2023-12-11 09:36:16 +0000

    Update notes
    Signed-off-by: ChenXiaoSong <chenxiaosongemail@foxmail.com>

--- src/chenxiaosong.com/restart.sh
+++ src/chenxiaosong.com/restart.sh
@@ -1,18 +1,31 @@
-src_path=/home/sonvhi/chenxiaosong/code/
-dst_path=/var/www/
-repalace_ip=172.20.26.131
+src_path=/home/sonvhi/chenxiaosong/code
+dst_path=/var/www
+repalace_ip=172.20.23.55
 
-cd ${src_path}pictures/
-git pull origin master
-cd ${src_path}blog/
-git pull origin master
-cd ${HOME}
+restart=false
 
-bash ${src_path}blog/src/chenxiaosong.com/link.sh
-bash ${src_path}blog/src/chenxiaosong.com/create-html.sh
-# # 如果部署在局域网，替换成局域网ip
-# find ${dst_path}html/ -type f -exec sed -i 's/chenxiaosong.com/'${repalace_ip}'/g' {} +
-# # default文件本来是个软链接，执行完sed后变成了文件
-# sed -i 's/chenxiaosong.com/'${repalace_ip}'/g' /etc/nginx/sites-enabled/default
-# iptables -F # 根据情况决定是否要清空防火墙规则
-service nginx restart
+update_repository() {
+    cd ${src_path}/${1}/
+    git fetch origin
+    local_head=$(git rev-parse HEAD)
+    origin_head=$(git rev-parse origin/master)
+    if [ "${local_head}" = "${origin_head}" ]; then
+        git pull origin master
+        restart=true
+    fi
+    cd -
+}
+
+update_repository pictures
+update_repository blog
+
+if [ restart = true ]; then
+    bash ${src_path}/blog/src/chenxiaosong.com/link.sh
+    bash ${src_path}/blog/src/chenxiaosong.com/create-html.sh
+    # # 如果部署在局域网，替换成局域网ip
+    # find ${dst_path}/html/ -type f -exec sed -i 's/chenxiaosong.com/'${repalace_ip}'/g' {} +
+    # # default文件本来是个软链接，执行完sed后变成了文件
+    # sed -i 's/chenxiaosong.com/'${repalace_ip}'/g' /etc/nginx/sites-enabled/default
+    # iptables -F # 根据情况决定是否要清空防火墙规则
+    service nginx restart
+fi
