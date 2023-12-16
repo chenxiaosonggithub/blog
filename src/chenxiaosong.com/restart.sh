4adaa25242ffbdc11224fbddedf335f4250669f6
ChenXiaoSong <chenxiaosongemail@foxmail.com>
2023-12-16 12:37:23 +0000

    Update notes
    Signed-off-by: ChenXiaoSong <chenxiaosongemail@foxmail.com>

--- src/chenxiaosong.com/restart.sh
+++ src/chenxiaosong.com/restart.sh
@@ -3,8 +3,9 @@
 src_path=/home/sonvhi/chenxiaosong/code
 dst_path=/var/www
 repalace_ip=172.20.23.55
+is_replace=false
 
-restart=false
+is_restart=false
 
 update_repository() {
     cd ${src_path}/${1}/
@@ -13,7 +14,8 @@ update_repository() {
     origin_head=$(git rev-parse origin/master)
     if [ "${local_head}" != "${origin_head}" ]; then
         timeout 20 git pull origin master
-        restart=true
+        # git push github master
+        is_restart=true
     fi
     cd -
 }
@@ -22,23 +24,26 @@ update_repository pictures
 update_repository blog
 
 add_common() {
-    sed -i '/<\/header>/,/<\/body>/!d' /var/www/html/common.html
-    sed -i '1d;$d' /var/www/html/common.html
-    # find ${dst_path}/html/ -type f -name '*.html' -exec sed -i -e '/<header/r /var/www/html/common.html' {} + # 针对所有文件
-    find ${dst_path}/html/ -type f -name '*.html' | grep -v ${dst_path}/html/index.html | xargs sed -i -e '/<header/r /var/www/html/common.html' # index文件除外
+    sed -i '/<\/header>/,/<\/body>/!d' /var/www/html/common.html # 只保留</header>到</body>的内容
+    sed -i '1d;$d' /var/www/html/common.html # 删除第一行和最后一行
+    # find ${dst_path}/html/ -type f -name '*.html' -exec sed -i -e '/<header/r ${dst_path}/html/common.html' {} + # 所有文件
+    find ${dst_path}/html/ -type f -name '*.html' | grep -v ${dst_path}/html/index.html \
+        | xargs sed -i -e '/<header/r ${dst_path}/html/common.html' # 在/<header之后插入common.html整个文件, index文件除外
 }
 
-if [ ${restart} = true ]; then
+if [ ${is_restart} = true ]; then
     echo "recreate html, restart service"
     bash ${src_path}/blog/src/chenxiaosong.com/link.sh
     bash ${src_path}/blog/src/chenxiaosong.com/create-html.sh
     add_common
-    # # 如果部署在局域网，替换成局域网ip
-    # find ${dst_path}/html/ -type f -name '*.html' -exec sed -i 's/chenxiaosong.com/'${repalace_ip}'/g' {} +
-    # # default文件本来是个软链接，执行完sed后变成了文件
-    # sed -i 's/chenxiaosong.com/'${repalace_ip}'/g' /etc/nginx/sites-enabled/default
-    # iptables -F # 根据情况决定是否要清空防火墙规则
-    service nginx restart
+    # 如果部署在局域网，替换成局域网ip
+    if [ ${is_replace} = true ]; then
+        find ${dst_path}/html/ -type f -name '*.html' -exec sed -i 's/chenxiaosong.com/'${repalace_ip}'/g' {} +
+        # default文件本来是个软链接，执行完sed后变成了文件
+        sed -i 's/chenxiaosong.com/'${repalace_ip}'/g' /etc/nginx/sites-enabled/default
+    fi
+    iptables -F # 根据情况决定是否要清空防火墙规则
+    service nginx is_restart
 else
     echo "no change"
 fi
