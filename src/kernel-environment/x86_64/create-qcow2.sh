1dd72aea1f1919da1dfda9731d7e4d3b8c849a65
ChenXiaoSong <chenxiaosongemail@foxmail.com>
2024-01-02 08:11:45 +0000

    Update notes
    Signed-off-by: ChenXiaoSong <chenxiaosongemail@foxmail.com>

--- src/kernel-environment/x86_64/create-qcow2.sh
+++ src/kernel-environment/x86_64/create-qcow2.sh
@@ -16,15 +16,19 @@ do
 		exit 1
 	fi
 
-	cp update-image.sh ${dst_path}/${element}.${image_type}/start.sh
+	cp update-base.sh ${dst_path}/${element}.${image_type}/start.sh
 	format_num=$(printf "%02d\n" ${element})
 	gdb_port=`expr 5550 + $element`
 	sed -i "s/00:11:22:33:44:55/00:11:22:33:44:${format_num}/g" ${dst_path}/${element}.${image_type}/start.sh
-	sed -i "s/${image_type}.qcow2.updating/image.qcow2/g" ${dst_path}/${element}.${image_type}/start.sh
+	sed -i "s/${image_type}.qcow2/image.qcow2/g" ${dst_path}/${element}.${image_type}/start.sh
 	echo "-drive file=1,if=none,format=raw,cache=writeback,file.locking=off,id=dd_1 \\" >> ${dst_path}/${element}.${image_type}/start.sh
 	echo "-device scsi-hd,drive=dd_1,id=disk_1,logical_block_size=4096,physical_block_size=4096 \\" >> ${dst_path}/${element}.${image_type}/start.sh
+	echo "-drive file=2,if=none,format=raw,cache=writeback,file.locking=off,id=dd_2 \\" >> ${dst_path}/${element}.${image_type}/start.sh
+	echo "-device scsi-hd,drive=dd_2,id=disk_2,logical_block_size=4096,physical_block_size=4096 \\" >> ${dst_path}/${element}.${image_type}/start.sh
 	echo "-drive file=nvme,if=none,format=raw,cache=writeback,file.locking=off,id=b_nvme_1 \\" >> ${dst_path}/${element}.${image_type}/start.sh
 	echo "-device nvme,drive=b_nvme_1,serial=d_b_nvme_1 \\" >> ${dst_path}/${element}.${image_type}/start.sh
+	echo "-drive file=nvme,if=none,format=raw,cache=writeback,file.locking=off,id=b_nvme_2 \\" >> ${dst_path}/${element}.${image_type}/start.sh
+	echo "-device nvme,drive=b_nvme_2,serial=d_b_nvme_2 \\" >> ${dst_path}/${element}.${image_type}/start.sh
 	echo "-gdb tcp::${gdb_port} \\" >> ${dst_path}/${element}.${image_type}/start.sh
 done
 
--- src/kernel-environment/x86_64/update-base.sh
+++ src/kernel-environment/x86_64/update-base.sh
@@ -7,12 +7,12 @@ qemu-system-x86_64 \
 -smp 16 \
 -m 4096 \
 -kernel /home/sonvhi/chenxiaosong/code/$kernel_version/build/arch/x86/boot/bzImage \
--virtfs local,id=kmod_dev,path=/home/sonvhi/sonvhi/home/sonvhi/chenxiaosong/,readonly,mount_tag=9p,security_model=none \
+-virtfs local,id=kmod_dev,path=/home/sonvhi/chenxiaosong/,readonly,mount_tag=9p,security_model=none \
 -vga none \
 -nographic \
 -append "nokaslr console=ttyS0 root=/dev/vda rw kmemleak=on" \
 -device virtio-scsi-pci \
--drive file=x86_64-bullseye.qcow2.updating,if=none,format=qcow2,cache=writeback,file.locking=off,id=root \
+-drive file=x86_64-bullseye.qcow2,if=none,format=qcow2,cache=writeback,file.locking=off,id=root \
 -device virtio-blk,drive=root,id=d_root \
 -net nic,model=virtio,macaddr=00:11:22:33:44:55 \
 -net bridge,br=virbr0 \
