e5fb2f89887275740d7f2713a9b1b141ea54b790
ChenXiaoSong <chenxiaosongemail@foxmail.com>
2024-01-08 07:27:05 +0000

    Update notes
    Signed-off-by: ChenXiaoSong <chenxiaosongemail@foxmail.com>

--- src/ssh-reverse/monitor-ssh.sh
+++ src/ssh-reverse/monitor-ssh.sh
@@ -3,13 +3,13 @@
 log_path=/tmp
 while true
 do
-	ssh -p 55555 -o ConnectTimeout=1 -q sonvhi@hz.chenxiaosong.com exit
+	ssh -p 55555 -o ConnectTimeout=2 -q sonvhi@hz.chenxiaosong.com exit
 	if [ $? != 0 ]
 	then
-		echo `date` >> ${log_path}/ssh-monitor-fail.log
+		echo `date` > ${log_path}/ssh-monitor-fail.log
 		systemctl restart ssh-reverse.service
 	else
-		echo `date` >> ${log_path}/ssh-monitor-success.log
+		echo `date` > ${log_path}/ssh-monitor-success.log
 	fi
 
 	sleep 30
