a27be18ac265bf0f1ac0b909ece8d14eef3ce602
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2024-05-11 10:07:34 +0800

    Update notes
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

--- src/chenxiaosong.com/copy-config.sh
+++ src/chenxiaosong.com/copy-config.sh
@@ -2,4 +2,4 @@ blog_path=/home/sonvhi/chenxiaosong/code/blog # 请替换为具体的路径
 config_file=/etc/nginx/sites-enabled/default
 rm ${config_file}
 cp ${blog_path}/src/chenxiaosong.com/nginx-config ${config_file}
-cat ${blog_path}/../private-blog/scripts/others-nginx-config > ${config_file}
+cat ${blog_path}/../private-blog/scripts/others-nginx-config >> ${config_file}
