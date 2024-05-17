051ffaf01bd77816346fa15a9f4132e6ae9ed267
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2024-05-17 10:46:13 +0800

    Linux内核书籍推荐 独立网页
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

--- src/chenxiaosong.com/create-html.sh
+++ src/chenxiaosong.com/create-html.sh
@@ -17,6 +17,7 @@ array=(
     ${tmp_courses_path}/kernel.md courses/kernel.html "Linux内核课程"
     ${tmp_courses_path}/kernel-introduction.md courses/kernel-introduction.html "Linux内核简介"
     ${tmp_courses_path}/kernel-dev-invironment.md courses/kernel-dev-invironment.html "Linux内核开发环境"
+    ${tmp_courses_path}/kernel-book.md courses/kernel-book.html "Linux内核书籍推荐"
     courses/book-contents.md courses/book-contents.html "书籍目录"
     # Linux内核
     src/kernel-environment/kernel-crash-vmcore.md kernel/kernel-crash-vmcore.html "crash解析vmcore"
