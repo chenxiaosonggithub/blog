02151b27b35c8d32b50eeebf6479f1fb69b1f467
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2024-05-17 18:17:57 +0800

    add common_lib.sh
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

---
+++ src/chenxiaosong.com/common_lib.sh
@@ -0,0 +1,71 @@
+# --standalone：此选项指示 pandoc 生成一个完全独立的输出文件，包括文档标题、样式表和其他元数据，使输出文件成为一个完整的文档。
+# --metadata encoding=gbk：这个选项允许您添加元数据。在这种情况下，您将 encoding 设置为 gbk，指定输出 HTML 文档的字符编码为 GBK。这对于确保生成的文档以正确的字符编码进行保存非常重要。
+# --toc：这个选项指示 pandoc 生成一个包含文档目录（Table of Contents，目录）的 HTML 输出。TOC 将包括文档中的章节和子章节的链接，以帮助读者导航文档。
+pandoc_common_options="--to html --standalone --metadata encoding=gbk --number-sections --css https://chenxiaosong.com/stylesheet.css"
+
+create_sign() {
+    local src_file=$1
+    local tmp_html_path=$2
+
+    local html_title="签名"
+    local dst_file=${tmp_html_path}/sign.html
+    local from_format="--from markdown"
+    pandoc ${src_file} -o ${dst_file} --metadata title="${html_title}" ${from_format} ${pandoc_common_options}
+    # 先去除sign.html文件中其他内容
+    sed -i '/<\/header>/,/<\/body>/!d' ${tmp_html_path}/sign.html # 只保留</header>到</body>的内容
+    sed -i '1d;$d' ${tmp_html_path}/sign.html # 删除第一行和最后一行
+}
+
+# 要定义数组array
+create_html() {
+    local src_path=$1
+    local tmp_html_path=$2
+
+    local element_count="${#array[@]}" # 总个数
+    local count_per_line=5
+    for ((index=0; index<${element_count}; index=$((index + ${count_per_line})))); do
+        local is_toc=${array[${index}]}
+        local is_sign=${array[${index}+1]}
+        local ifile=${array[${index}+2]}
+        local ofile=${array[${index}+3]}
+        local html_title=${array[${index}+4]}
+        local pandoc_options=${pandoc_common_options}
+
+        local src_file=${src_path}/${ifile} # 相对路径
+        echo "create ${ofile}"
+        if [[ ${ifile} == '/'* ]]; then
+            src_file=${ifile} # 绝对路径
+        fi
+        local dst_file=${tmp_html_path}/${ofile} # 生成的html文件名
+        local dst_dir="$(dirname "${dst_file}")" # html文件所在的文件夹
+        if [ ! -d "${dst_dir}" ]; then
+            mkdir -p "${dst_dir}" # 文件夹不存在就创建
+        fi
+        from_format="--from markdown"
+        if [[ ${src_file} == *.rst ]]; then
+            from_format="--from rst" # rst格式
+        fi
+        if [[ ${is_toc} == 1 ]]; then
+            pandoc_options="${pandoc_options} --toc"
+        fi
+        pandoc ${src_file} -o ${dst_file} --metadata title="${html_title}" ${from_format} ${pandoc_options}
+        if [[ ${is_sign} == 1 ]]; then
+            # 在<header之后插入sign.html整个文件
+            sed -i -e '/<header/r '${tmp_html_path}'/sign.html' ${dst_file} # index文件除外
+        fi
+    done
+}
+
+change_perm() {
+    local html_path=$1
+
+    chown -R www-data:www-data ${html_path}/
+
+    # -type f：这个选项告诉 find 只搜索普通文件（不包括目录和特殊文件）。
+    # -exec chmod 400 {} +：这个部分告诉 find 对每个找到的文件执行 chmod 400 操作。{} 表示找到的文件的占位符，+ 表示一次处理多个文件以提高效率。
+    find ${html_path}/ -type f -exec chmod 400 {} +
+
+    # -type d：这个选项告诉find只搜索目录（不包括普通文件）。
+    find ${html_path}/ -type d -exec chmod 500 {} +
+}
+
--- src/chenxiaosong.com/create-html.sh
+++ src/chenxiaosong.com/create-html.sh
@@ -4,6 +4,8 @@ tmp_html_path=${dst_path}/html-tmp
 html_path=${dst_path}/html
 tmp_courses_path=/tmp/blog-courses
 
+. ${src_path}/blog/src/chenxiaosong.com/common_lib.sh
+
 # 每一行代表： 是否生成目录 是否添加签名 markdown或rst文件相对路径 html文件相对路径 网页标题
 array=(
     # 自我介绍
@@ -101,11 +103,6 @@ array=(
     1 1 src/private/chatgpt/chatgpt.md private/chatgpt.html "注册ChatGPT"
 )
 
-# --standalone：此选项指示 pandoc 生成一个完全独立的输出文件，包括文档标题、样式表和其他元数据，使输出文件成为一个完整的文档。
-# --metadata encoding=gbk：这个选项允许您添加元数据。在这种情况下，您将 encoding 设置为 gbk，指定输出 HTML 文档的字符编码为 GBK。这对于确保生成的文档以正确的字符编码进行保存非常重要。
-# --toc：这个选项指示 pandoc 生成一个包含文档目录（Table of Contents，目录）的 HTML 输出。TOC 将包括文档中的章节和子章节的链接，以帮助读者导航文档。
-pandoc_common_options="--to html --standalone --metadata encoding=gbk --number-sections --css https://chenxiaosong.com/stylesheet.css"
-
 init_begin() {
     mkdir -p ${tmp_html_path}
     bash ${src_path}/blog/courses/remove-private.sh
@@ -114,53 +111,6 @@ init_begin() {
 init_end() {
     rm ${html_path}/ -rf
     mv ${tmp_html_path} ${html_path}
-}
-
-create_sign() {
-    src_file=${src_path}/blog/src/self-introduction/sign.md
-    html_title="签名"
-    dst_file=${tmp_html_path}/sign.html
-    from_format="--from markdown"
-    pandoc ${src_file} -o ${dst_file} --metadata title="${html_title}" ${from_format} ${pandoc_common_options}
-    # 先去除sign.html文件中其他内容
-    sed -i '/<\/header>/,/<\/body>/!d' ${tmp_html_path}/sign.html # 只保留</header>到</body>的内容
-    sed -i '1d;$d' ${tmp_html_path}/sign.html # 删除第一行和最后一行
-}
-
-create_html() {
-    create_sign
-    element_count="${#array[@]}" # 总个数
-    count_per_line=5
-    for ((index=0; index<${element_count}; index=$((index + ${count_per_line})))); do
-        is_toc=${array[${index}]}
-        is_sign=${array[${index}+1]}
-        ifile=${array[${index}+2]}
-        ofile=${array[${index}+3]}
-        html_title=${array[${index}+4]}
-        pandoc_options=${pandoc_common_options}
-
-        src_file=${src_path}/blog/${ifile} # 相对路径
-        if [[ ${ifile} == '/'* ]]; then
-            src_file=${ifile} # 绝对路径
-        fi
-        dst_file=${tmp_html_path}/${ofile} # 生成的html文件名
-        dst_dir="$(dirname "${dst_file}")" # html文件所在的文件夹
-        if [ ! -d "${dst_dir}" ]; then
-            mkdir -p "${dst_dir}" # 文件夹不存在就创建
-        fi
-        from_format="--from markdown"
-        if [[ ${src_file} == *.rst ]]; then
-            from_format="--from rst" # rst格式
-        fi
-        if [[ ${is_toc} == 1 ]]; then
-            pandoc_options="${pandoc_options} --toc"
-        fi
-        pandoc ${src_file} -o ${dst_file} --metadata title="${html_title}" ${from_format} ${pandoc_options}
-        if [[ ${is_sign} == 1 ]]; then
-            # 在<header之后插入sign.html整个文件
-            sed -i -e '/<header/r '${tmp_html_path}'/sign.html' ${dst_file} # index文件除外
-        fi
-    done
 }
 
 copy_secret_repository() {
@@ -173,20 +123,10 @@ copy_public_files() {
     cp ${src_path}/blog/src/chenxiaosong.com/stylesheet.css ${tmp_html_path}/
 }
 
-change_perm() {
-    chown -R www-data:www-data ${tmp_html_path}/
-
-    # -type f：这个选项告诉 find 只搜索普通文件（不包括目录和特殊文件）。
-    # -exec chmod 400 {} +：这个部分告诉 find 对每个找到的文件执行 chmod 400 操作。{} 表示找到的文件的占位符，+ 表示一次处理多个文件以提高效率。
-    find ${tmp_html_path}/ -type f -exec chmod 400 {} +
-
-    # -type d：这个选项告诉find只搜索目录（不包括普通文件）。
-    find ${tmp_html_path}/ -type d -exec chmod 500 {} +
-}
-
 init_begin
-create_html
+create_sign ${src_path}/blog/src/self-introduction/sign.md ${tmp_html_path}
+create_html ${src_path}/blog ${tmp_html_path}
 copy_secret_repository
 copy_public_files
-change_perm
+change_perm ${tmp_html_path}
 init_end
