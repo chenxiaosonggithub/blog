049b41a7e32ff6a5820e12b8e74135d3a184bcc2
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2025-03-13 23:43:18 +0800

    update create-html.sh
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

--- src/blog-web/create-html.sh
+++ src/blog-web/create-html.sh
@@ -24,6 +24,7 @@ my_init() {
 	rm -rf $(comm_tmp_src_path)
 	mkdir -p $(comm_tmp_src_path)
 	cp -rf ${src_path}/* $(comm_tmp_src_path)
+	ln -s ${src_path}/../tmp $(comm_tmp_src_path)/tmp
 	bash ${src_path}/course/course.sh
 	comm_rm_private $(comm_tmp_src_path)
 }
