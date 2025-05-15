ffe1b86c354c3da98af66d4f892a763b446ffc14
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2025-05-15 21:54:47 +0800

    update course/kernel/src/aarch64/update-base.sh
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

--- course/kernel/src/aarch64/update-base.sh
+++ course/kernel/src/aarch64/update-base.sh
@@ -7,7 +7,7 @@ qemu-system-aarch64 \
 -machine virt \
 -cpu cortex-a72 \
 -kernel ${MY_CODE_TOP_PATH}/$kernel_version/aarch64-build/arch/arm64/boot/Image \
---virtfs local,id=kmod_dev,path=${MY_TOP_PATH},security_model=none,mount_tag=9p \
+--virtfs local,id=kmod_dev,path=${MY_CODE_TOP_PATH},security_model=none,mount_tag=9p \
 -device virtio-scsi-pci \
 -net nic,model=virtio,macaddr=00:11:22:33:44:55 \
 -net tap \
