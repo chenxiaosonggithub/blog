02a6e1860019d03263ab039449b36d98b1790e05
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2025-05-15 16:55:36 +0800

    update mod-cfg.sh
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

--- course/kernel/src/mod-cfg.sh
+++ course/kernel/src/mod-cfg.sh
@@ -5,7 +5,7 @@ fi
 
 build_dir=x86_64-build
 mnt_point=/tmp/9p
-knl_base_dir=${mnt_point}/code/$1 # 修改成内核代码目录
+knl_base_dir=${mnt_point}/$1 # 修改成内核代码目录
 mkdir $mnt_point
 mkdir /lib/modules -p
 mount -t 9p -o trans=virtio 9p $mnt_point
