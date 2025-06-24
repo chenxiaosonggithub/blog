a5f8bb21c5725ede9ed4948b108df80d65fda0d0
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2025-06-24 14:04:35 +0000

    update push-web.sh
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

--- src/blog-web/push-web.sh
+++ src/blog-web/push-web.sh
@@ -41,4 +41,4 @@ git branch -m master # 确保分支名为master
 git push origin master -f
 
 # others blog
-bash ${code_path}/private-blog/others-blog/push-github.sh "${is_new_repo}"
+# bash ${code_path}/private-blog/others-blog/push-github.sh "${is_new_repo}"
