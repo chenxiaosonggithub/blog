ae63cc0ab4290f24d59878b45fcd31b94bcbec52
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2025-10-31 18:19:59 +0800

    update set_proxy.sh
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

--- course/gnu-linux/src/config-file/set_proxy.sh
+++ course/gnu-linux/src/config-file/set_proxy.sh
@@ -1,13 +1,16 @@
-if [[ $1 == 1 ]]
-then
+if [[ $1 == 1 ]]; then
 	echo "set proxy"
 	export http_proxy=http://172.17.0.1:1081
 	export https_proxy=http://172.17.0.1:1081
-elif [[ $1 == 0 ]]
-then
+	echo "http_proxy=${http_proxy}"
+	echo "https_proxy=${https_proxy}"
+elif [[ $1 == 0 ]]; then
 	echo "unset proxy"
 	export http_proxy=
 	export https_proxy=
+elif [[ $1 == get ]]; then
+	echo "http_proxy=${http_proxy}"
+	echo "https_proxy=${https_proxy}"
 else
 	echo "wrong parameter"
 fi
