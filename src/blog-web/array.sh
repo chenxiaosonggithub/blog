cacfc767bc85a720286cf46e52a94f1ce442b329
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2025-11-16 13:11:22 +0800

    update smb test README
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

--- src/blog-web/array.sh
+++ src/blog-web/array.sh
@@ -298,6 +298,6 @@ comm_array=(
 	1 2 en/null-ptr-deref-in-nfsd4_probe_callback.md ~ "null-ptr-deref in nfsd4_probe_callback()"
 	1 2 en/smb2-change-notify.md ~ "SMB2 CHANGE_NOTIFY feature"
 	1 2 en/smb-test.md ~ "How to Test SMB"
-	1 2 tmp/en/smb-test/20251109.md en/smb-test/20251109.html "SMB Test Results on 2025-11-09"
-	1 2 tmp/en/smb-test/20251113.md en/smb-test/20251113.html "SMB Test Results on 2025-11-13"
+	1 2 tmp/smb/test/20251109/README.md en/smb-test-20251109.html "SMB Test Results on 2025-11-09"
+	1 2 tmp/smb/test/20251113/README.md en/smb-test-20251113.html "SMB Test Results on 2025-11-13"
 )
