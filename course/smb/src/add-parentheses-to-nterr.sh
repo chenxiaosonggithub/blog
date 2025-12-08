749e5438b7f6cdd055fdf013e2b85c0ab257acaf
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2025-12-08 10:52:41 +0800

    update add-parentheses-to-nterr.sh
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

--- course/smb/src/add-parentheses-to-nterr.sh
+++ course/smb/src/add-parentheses-to-nterr.sh
@@ -1,3 +1,3 @@
 sed -i '/|/s/ 0x/ (0x/' fs/smb/client/nterr.h
 sed -i '/|/ { /.*\*\/$/! s/$/)/ }' fs/smb/client/nterr.h
-sed -i '/|/ s/[[:space:]]*\/\*/) &/' fs/smb/client/nterr.h
+sed -i '/|/ s/[[:space:]]*\/\*/)&/' fs/smb/client/nterr.h
