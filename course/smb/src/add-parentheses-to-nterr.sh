a1c4b3fa220107a5ac88d74a8c8f941ae50b3e2f
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2025-12-08 10:27:16 +0800

    update add-parentheses-to-nterr.sh
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

--- course/smb/src/add-parentheses-to-nterr.sh
+++ course/smb/src/add-parentheses-to-nterr.sh
@@ -1,16 +1,3 @@
-while IFS= read -r line; do
-	if echo "$line" | grep -q "|"; then
-		# Add "("
-		line=$(echo "$line" | sed "s/ 0x/ (0x/")
-		if [[ "$line" != *"*/" ]]; then
-			# Add ")" if line does not end with a comment
-			line=$(echo "$line" | sed "s/$/)/")
-		else
-			# Add ")" if line end with a comment
-			line=$(echo "$line" | sed 's/[[:space:]]*\/\*/) &/')
-		fi
-	fi
-	echo "$line"
-done < fs/smb/client/nterr.h > fs/smb/client/nterr.h.tmp
-
-mv fs/smb/client/nterr.h.tmp fs/smb/client/nterr.h
+sed -i '/|/s/ 0x/ (0x/' fs/smb/client/nterr.h
+sed -i '/|/ { /.*\*\/$/! s/$/)/ }' fs/smb/client/nterr.h
+sed -i '/|/ s/[[:space:]]*\/\*/) &/' fs/smb/client/nterr.h
