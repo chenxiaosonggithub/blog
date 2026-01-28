e5a0febc39efb2944dca09b8dee9dadd5534e30d
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2026-01-28 06:19:11 +0000

    update samba-svr-setup.sh
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

--- course/smb/src/samba-svr-setup.sh
+++ course/smb/src/samba-svr-setup.sh
@@ -1,22 +1,22 @@
-systemctl stop ksmbd.service
-
 mkfs.ext4 -b 4096 -F /dev/sda
 mkfs.ext4 -b 4096 -F /dev/sdb
 
 mkdir /tmp/s_test
-mkdir /tmp/s_scratch
+mkdir /tmp/s_test2
+mkdir /tmp/s_test3
 
 mount -t ext4 /dev/sda /tmp/s_test
-mount -t ext4 /dev/sdb /tmp/s_scratch
+mount -t ext4 /dev/sdb /tmp/s_test2
 
 systemctl stop firewalld
 setenforce 0
 
-chmod 777 /tmp/s_test
-chmod 777 /tmp/s_scratch
-
+systemctl stop ksmbd
 systemctl restart smbd.service # debian
 systemctl restart smb.service # fedora
 
+chmod 777 /tmp/s_test
+chmod 777 /tmp/s_test2
+
 mkdir /tmp/test
-mkdir /tmp/scratch
+mkdir /tmp/test2
