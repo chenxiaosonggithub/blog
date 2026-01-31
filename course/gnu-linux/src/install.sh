02e93b4d82883fe27b176fb2d1bde460e3d2afb0
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2026-01-31 13:56:38 +0800

    update install.sh
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

--- course/gnu-linux/src/install.sh
+++ course/gnu-linux/src/install.sh
@@ -1,14 +1,22 @@
-blog_path=/home/chenxiaosong/code/blog
-. $blog_path/src/blog-web/repos.sh
+if [ $# -ne 2 ]; then
+	echo "用法: $0 <fedora/ubuntu> <physical/docker/vm>"
+	exit 1
+fi
+distribution=$1
+machine=$2
 
-clone_repos()
+code_path=/home/chenxiaosong/code/
+. $code_path/blog/src/blog-web/repos.sh
+
+clone_all_repos()
 {
 	local element_count="${#repos_array[@]}" # 总个数
 	local count_per_line=2
 	for ((index=0; index<${element_count}; index=$((index + ${count_per_line})))); do
 		# is_push_github=${repos_array[${index}]}
 		local repo=${repos_array[${index}+1]}
-		if [ ! -d "$blog_path/$repo" ]; then
+		if [ ! -d "$code_path/$repo" ]; then
+			cd $code_path
 			git clone -o gitee git@gitee.com:chenxiaosonggitee/$repo.git
 		fi
 	done
@@ -16,14 +24,23 @@ clone_repos()
 
 cp_config_file()
 {
-	bash ${blog_path}/course/gnu-linux/src/config-file/cp-to-home.sh
+	cd ${code_path}/blog/course/gnu-linux/src/config-file
+	bash cp-to-home.sh
 }
 
 fedora_physical()
 {
 	sudo dnf install -y ibus*wubi* openssh-server vim virt-manager git
 	cp_config_file
-	clone_repos
+	clone_all_repos
 }
 
+case "$distribution-$machine" in
+fedora-physical)
+	fedora_physical
+	;;
+*)
+	echo "Invalid argument: $distribution $machine"
+	;;
+esac
 
