8014c3d324d792356d98daeb5329a9f4a681ae88
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2026-04-09 08:58:14 +0000

    update aarch64/create-qcow2.sh
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

--- course/kernel/src/aarch64/create-qcow2.sh
+++ course/kernel/src/aarch64/create-qcow2.sh
@@ -1,7 +1,7 @@
-array=(5 6)
+array=(3 4)
 image_type=`basename $(pwd)`
+qcow2_file=$(ls *.qcow2)
 dst_path=$(pwd)/../../vm/
-idx=0
 
 for element in ${array[@]}
 do
@@ -11,7 +11,7 @@ do
 		exit 1
 	fi
 	rm ${dst_path}/${element}.${image_type}/image.qcow2 -rf
-	qemu-img create -F qcow2 -b $(pwd)/${image_type}.qcow2 -f qcow2 ${dst_path}${element}.${image_type}/image.qcow2
+	qemu-img create -F qcow2 -b $(pwd)/${qcow2_file} -f qcow2 ${dst_path}${element}.${image_type}/image.qcow2
 	if [ $? -ne 0 ]
 	then
 		exit 1
@@ -24,8 +24,8 @@ do
 	sed -i "s/${image_type}.qcow2/image.qcow2/g" ${dst_path}/${element}.${image_type}/start.sh
 	echo "-drive file=1,if=none,format=raw,cache=writeback,file.locking=off,id=dd_1 \\" >> ${dst_path}/${element}.${image_type}/start.sh
 	echo "-device scsi-hd,drive=dd_1,id=disk_1,logical_block_size=4096,physical_block_size=4096 \\" >> ${dst_path}/${element}.${image_type}/start.sh
-	echo "-drive file=nvme1,if=none,format=raw,cache=writeback,file.locking=off,id=b_nvme_1 \\" >> ${dst_path}/${element}.${image_type}/start.sh
-	echo "-device nvme,drive=b_nvme_1,serial=d_b_nvme_1 \\" >> ${dst_path}/${element}.${image_type}/start.sh
+	# echo "-drive file=nvme1,if=none,format=raw,cache=writeback,file.locking=off,id=b_nvme_1 \\" >> ${dst_path}/${element}.${image_type}/start.sh
+	# echo "-device nvme,drive=b_nvme_1,serial=d_b_nvme_1 \\" >> ${dst_path}/${element}.${image_type}/start.sh
 	echo "-gdb tcp::${gdb_port} \\" >> ${dst_path}/${element}.${image_type}/start.sh
 done
 
