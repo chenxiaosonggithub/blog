e76929b8603a48fbde7a9ef2015d07c638600dcb
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2026-05-22 18:12:37 +0800

    update cpolar/
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

---
+++ course/gnu-linux/src/cpolar/scp-tianyi.sh
@@ -0,0 +1,17 @@
+if [ $# -ne 2 ]; then
+	echo "用法: bash $0 <from/to> <文件名(远程机器~/forVM目录)>"
+	exit 1
+fi
+from_or_to=$1
+
+tunnel_name=tianyi # 隧道名称
+script_dir=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
+. $script_dir/common/common.sh
+
+case $from_or_to in
+from)
+	scp -P $port $ssh_user@$address:/home/chenxiaosong/forVM/$2 .
+	;;
+to)
+	scp -P $port $2 $ssh_user@$address:/home/chenxiaosong/forVM/
+esac
