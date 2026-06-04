15b0b408a32a29c39b6f87bb6c512bc6b70879b7
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2026-06-04 08:41:35 +0800

    update cpolar
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

--- course/gnu-linux/src/cpolar/ssh-tianyi.sh
+++ course/gnu-linux/src/cpolar/ssh-tianyi.sh
@@ -2,5 +2,10 @@ tunnel_name=tianyi # 隧道名称
 script_dir=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
 . $script_dir/common.sh
 
-do_ssh
+if [ $# -eq 1 ]; then
+	# do_ssh ". eth-aorus.sh $@; exec bash -l"
+	do_ssh ". eth-aorus.sh $@"
+else
+	do_ssh
+fi
 
