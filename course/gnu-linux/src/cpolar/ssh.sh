9d719b43f1f14c0d3bae08710ce528cef4d26d05
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2026-07-12 12:53:01 +0800

    update cpolar
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

--- course/gnu-linux/src/cpolar/ssh.sh
+++ course/gnu-linux/src/cpolar/ssh.sh
@@ -2,18 +2,29 @@ script_dir=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
 
 case "$1" in
 1)
-	bash $script_dir/aorus-ssh.sh aorus pm
+	session="tianyi"
+	bash $script_dir/tianyi-ssh.sh "
+	export LC_ALL=en_US.UTF-8; \
+	if tmux has-session -t $session 2>/dev/null; then \
+		tmux att -t $session; \
+	else \
+		tmux new -t $session; \
+	fi \
+	"
 	;;
 2)
-	bash $script_dir/aorus-ssh.sh code
+	bash $script_dir/aorus-ssh.sh aorus pm
 	;;
 3)
-	bash $script_dir/aorus-ssh.sh build
+	bash $script_dir/aorus-ssh.sh code
 	;;
 4)
-	bash $script_dir/aorus-ssh.sh qemu01
+	bash $script_dir/aorus-ssh.sh build
 	;;
 5)
+	bash $script_dir/aorus-ssh.sh qemu01
+	;;
+6)
 	bash $script_dir/aorus-ssh.sh qemu02
 	;;
 *)
