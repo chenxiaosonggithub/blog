f97106d8937782b88eba64822bde01b7df5e818c
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2026-08-09 11:22:06 +0800

    update cpolar kill-ssh.sh
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

--- course/gnu-linux/src/cpolar/kill-ssh.sh
+++ course/gnu-linux/src/cpolar/kill-ssh.sh
@@ -1,1 +1,2 @@
-ps | awk '$NF ~ /\/usr\/bin\/ssh$/ {print $1}' | xargs -r kill
+# ps | awk '$NF ~ /\/usr\/bin\/ssh$/ {print $1}' | xargs -r kill # 不能用，第一列可能是S或O，不一定为空
+ps | awk '$NF == "/usr/bin/ssh" {print ($1 ~ /^[0-9]+$/ ? $1 : $2)}' | xargs -r kill
