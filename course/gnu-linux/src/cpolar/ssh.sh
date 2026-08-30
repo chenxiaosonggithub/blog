404107b0ddd45b3fe4df7bf068d3a9e2801f0d75
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2026-08-30 16:51:32 +0800

    update cpolar
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

--- course/gnu-linux/src/cpolar/ssh.sh
+++ course/gnu-linux/src/cpolar/ssh.sh
@@ -1,11 +1,4 @@
 script_dir=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
 
-case "$1" in
-1|2|3|4|5|6|7)
-	bash "$script_dir/aorus-ssh.sh" "$1"
-	;;
-*)
-	echo "用法: bash $0 <1~5>"
-	;;
-esac
+bash "$script_dir/aorus-ssh.sh" "$1"
 	
