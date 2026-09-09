3d143d4b85c2d15d8f8d0a55ccbb538329e3013d
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2026-09-09 14:21:02 +0800

    update check-contribution.sh
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

--- course/kernel/src/check-contribution.sh
+++ course/kernel/src/check-contribution.sh
@@ -3,10 +3,9 @@ blog_path=${MY_CODE_TOP_PATH}/blog
 
 kernel_contrib_file=${blog_path}/course/kernel/contribution.md
 smb_contrib_file=${blog_path}/en/smb-contribution.md
-smb_review_file=${blog_path}/en/smb-review.md
-# echo $kernel_contrib_file $smb_contrib_file $smb_review_file
+# echo $kernel_contrib_file $smb_contrib_file
 
-find_smb_review_missing() {
+find_review_missing() {
 	local full_commit=$1
 	local subject=$2
 	local commit_id=$3
@@ -15,20 +14,19 @@ find_smb_review_missing() {
 	local ack_tag=$(git show $commit_id | grep -E "Acked-by:[[:space:]]*ChenXiaoSong")
 
 	if [ -z "$review_tag" ] && [ -z "$ack_tag" ]; then
-		return
-	fi
-
-	if grep -Fq "$full_commit" "$smb_review_file"; then
-		return
+		return 1
 	fi
 
 	local patch_link="[$full_commit](https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=$commit_id)"
 	if [ -n "$review_tag" ]; then
-		echo "[Review MISSING] - Review: $patch_link"
+		echo "- Review: $patch_link"
+		return 0
 	fi
 	if [ -n "$ack_tag" ]; then
-		echo "[Ack MISSING] - Ack: $patch_link"
+		echo "- Ack: $patch_link"
+		return 0
 	fi
+	return 1
 }
 
 find_missing() {
@@ -36,8 +34,6 @@ find_missing() {
 	local subject=$(echo "$full_commit" | cut -d' ' -f2-)
 	local commit_id=$(echo "$full_commit" | cut -d' ' -f1)
 	# echo $commit_id $subject
-
-	find_smb_review_missing "$full_commit" "$subject" "$commit_id"
 
 	if grep -Fq "$full_commit" "$kernel_contrib_file"; then
 		# echo "[FOUND] $subject"
@@ -48,7 +44,12 @@ find_missing() {
 		return
 	fi
 
-	echo "[PATCH MISSING] - [$full_commit](https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=$commit_id)"
+	find_review_missing "$full_commit" "$subject" "$commit_id"
+	if [ $? -eq 0 ]; then
+		return
+	fi
+
+	echo "- [$full_commit](https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=$commit_id)"
 }
 
 delete_commit_id() {
